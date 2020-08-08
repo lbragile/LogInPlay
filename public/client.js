88e2018bcc55560b4e6518eec88ba6788cb4cbf3
Lior Bragilevsky <lbragile@sfu.ca>
2020-08-08 16:01:48 -0700

    made the board and basic socket communication

--- public/client.js
+++ public/client.js
@@ -1,6 +1,26 @@
-if (!navigator.cookieEnabled) {
-  // The browser does not support or is blocking cookies from being set.
-  console.log("cookies not enabled");
-} else {
-  console.log("cookies are enabled");
+const socket = io();
+
+socket.on("connection", console.log("connected new socket"));
+
+socket.on("welcome", (id) => {
+  console.log("Welcome:" + id);
+});
+
+var $board = $("#board");
+var offset = 10,
+  counter = 0;
+for (let i = 0; i < 64; i++) {
+  let row = Math.floor(i / 8),
+    col = i % 8;
+
+  if ((row + col) % 2) {
+    counter++;
+    if (i < 24) {
+      let red_piece = $("#red-" + counter);
+      red_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
+    } else if (i > 39) {
+      let green_piece = $("#green-" + (counter - 20));
+      green_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
+    }
+  }
 }
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require("express");
+var socket = require("socket.io");
 var app = express();
 
 var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
-app.use(express.static("public"));
+var server = app.listen(port, () => console.log(`listening on port ${port}`));
+const io = socket(server);
+
+app.use(express.static("public")); // calls index.html which calls client.js at the end so io will be defined there
+
+io.on("connection", (socket) => {
+  socket.emit("welcome", socket.id);
+});
