0f81caacd43c2928756e4eae67a7b04b5366e526
Lior Bragilevsky <lbragile@sfu.ca>
2020-08-08 23:10:58 -0700

    pieces move and are seen by other user

--- public/client.js
+++ public/client.js
@@ -1,49 +1,45 @@
 // board setup
-var $board = $("#board");
-var offset = 10,
-  counter = 0;
-for (let i = 0; i < 64; i++) {
-  let row = Math.floor(i / 8),
-    col = i % 8;
+function boardInit(color) {
+  var offset = 10,
+    counter = 0;
+  for (let i = 0; i < 64; i++) {
+    let row = Math.floor(i / 8),
+      col = i % 8;
 
-  if ((row + col) % 2) {
-    counter++;
-    if (i < 24) {
-      let red_piece = $("#red-" + counter);
-      red_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
-    } else if (i > 39) {
-      let green_piece = $("#green-" + (counter - 20));
-      green_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
+    if ((row + col) % 2) {
+      counter++;
+      if (i < 24) {
+        let first_piece = $(`#${color.first}-${counter}`);
+        first_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
+      } else if (i > 39) {
+        let second_piece = $(`#${color.second}-${counter - 20}`);
+        second_piece.css({ top: row * 100 + offset, left: col * 100 + offset });
+      }
     }
   }
 }
 
 // move functionality
 function makeMove(turn) {
-  $(".piece").draggable({
+  $("." + turn).draggable({
     containment: "#board",
     grid: [100, 100],
   });
 
-  // drop on light
-  $(".light").droppable({
+  // drop on dark
+  $(".dark").droppable({
     drop: (event, ui) => {
       ui.draggable.draggable("option", "revert", false);
     },
   });
 
-  // revert on black
-  $(".dark").droppable({
+  // revert on light
+  $(".light").droppable({
     drop: (event, ui) => {
       ui.draggable.draggable("option", "revert", true);
     },
   });
-
-  // return turn == "green" ? "red" : "green";
 }
-
-var turn = "green";
-makeMove(turn);
 
 // socket connection/communication
 const socket = io();
@@ -61,4 +57,53 @@ $button.on("click", () => {
   socket.emit("search", socket.id);
 });
 
-socket.on("found", (message) => console.log(message));
+boardInit({ first: "red", second: "green" }); // display something before an event happens
+
+socket.on("found", (message) => {
+  $button.hide();
+  let color = { first: "red", second: "green" }; // default values
+
+  if (message.includes("green")) {
+    color.first = "green";
+    color.second = "red";
+  }
+
+  boardInit(color);
+  makeMove("green");
+  // alert(message);
+});
+
+// var green_move = true,
+//   red_move = false;
+
+$(".piece").on("mouseup", function () {
+  let piece_coords = [];
+  $.each($(".piece"), (index, value) => {
+    let data = {
+      id: value.id,
+      top: $(value).css("top"),
+      left: $(value).css("left"),
+      socket: socket.id,
+    };
+    piece_coords.push(data);
+  });
+
+  socket.emit("move", piece_coords);
+});
+
+socket.on("position", (data) => {
+  data.forEach((element) => {
+    $("#" + element.id).css({
+      top: element.top,
+      left: element.left,
+    });
+  });
+
+  let degrees = 180;
+  $("#board").css({
+    "-webkit-transform": "rotate(" + degrees + "deg)",
+    "-moz-transform": "rotate(" + degrees + "deg)",
+    "-ms-transform": "rotate(" + degrees + "deg)",
+    transform: "rotate(" + degrees + "deg)",
+  });
+});
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = socket(server);
 app.use(express.static("public")); // calls index.html which calls client.js at the end so io will be defined there
 
 var player_id = []; // increases when player presses 'play'
-
+var num_players = 0;
 io.on("connection", (socket) => {
   socket.emit("welcome", socket.id);
 
@@ -19,11 +19,35 @@ io.on("connection", (socket) => {
     }
 
     if (player_id.length % 2 == 0) {
-      io.to(player_id[0]).to(player_id[1]).emit("found", "Player found!");
-      io.to(player_id[0]).emit("found", "You are green");
-      io.to(player_id[1]).emit("found", "You are red");
+      //   io.to(player_id[0]).to(player_id[1]).emit("found", "Player found!"); // send a message to both
+      io.to(player_id[num_players]).emit(
+        "found",
+        "Player found! You are green."
+      );
+      io.to(player_id[num_players + 1]).emit(
+        "found",
+        "Player found! You are red."
+      );
+
+      num_players += 2;
     }
 
-    // console.log({ player_id });
+    console.log({ player_id });
+  });
+
+  socket.on("move", (piece_coords) => {
+    let which_socket;
+    if (piece_coords.socket == player_id[num_players - 2]) {
+      which_socket = player_id[num_players - 1];
+    } else {
+      which_socket = player_id[num_players - 2];
+    }
+    io.to(which_socket).emit("position", piece_coords); // send a message to both
+  });
+
+  socket.on("disconnect", function () {
+    // remove the player id from the search list
+    let index = player_id.indexOf(socket.id);
+    player_id.splice(index, 1);
   });
 });
